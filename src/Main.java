e0c0c1c07121c3b6e6bf36cfbf6d43c9708675c0
jlaurenzo <pazlaurencejohn@gmail.com>
2026-03-09 18:08:48 +0800

    Added, display note and view note functionality

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,50 @@ public class Main {
 
     public static void main (String args[]) {
 
+        String tryAgain = "yes";
+
         Scanner inputScanner = new Scanner(System.in);
         NoteManager newManager = new NoteManager();
 
+        while (tryAgain.equalsIgnoreCase("yes")) {
 
         System.out.println("---------- JOTTER ----------");
         System.out.println("1. Create New Note");
         System.out.println("2. View All Notes");
-        System.out.println("3. Search Notes");
+        System.out.println("3. Read Note");
         System.out.println("4. Edit Note");
         System.out.println("5. Delete Note");
         System.out.println("6. Exit");
         endLine();
         System.out.print("Enter Choice: ");
         int choice = inputScanner.nextInt();
+        inputScanner.nextLine();
 
-        switch (choice) {
-            case 1:
-                System.out.println("Create New Note");
-                newManager.CreateNewNote();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
+            switch (choice) {
+                case 1:
+                    System.out.println("Create New Note");
+                    newManager.CreateNewNote();
+                    break;
+                case 2:
+                    System.out.println("View All Notes");
+                    newManager.DisplayAllNote();
+                    break;
+                case 3:
+                    System.out.println("Read Note");
+                    newManager.ReadNote();
+                    break;
+                default:
+                    System.out.println("Error!");
 
+            }
 
-
+            System.out.print("Try again? (yes / no): ");
+            tryAgain = inputScanner.nextLine();
 
         }
 
-
-
-
-
     }
+
 
     static void endLine() {
         System.out.println("----------------------------");
--- src/NoteManager.java
+++ src/NoteManager.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class NoteManager {
 
     void CreateNewNote() {
-
         databaseManager newManager = new databaseManager();
         if (!newManager.database.exists()) {
             newManager.createDatabase();
@@ -11,19 +10,35 @@ public class NoteManager {
         Note newNote = new Note();
 
         Scanner userInput = new Scanner(System.in);
-
+        System.out.println("----------------------------");
         System.out.print("Enter Title: ");
         String title = userInput.nextLine();
         System.out.print("Enter Note: ");
         String note = userInput.nextLine();
-        userInput.close();
+        System.out.println("----------------------------");
+
+        int noteId = GetNextId() ;
 
         try (FileWriter noteWriter = new FileWriter("notes.txt", true)){
-            noteWriter.write("#" + newNote.getPrivateId() + " , " + newNote.createdDate + " , " + title + " , " + note + " , " + "\n");
+            noteWriter.write(noteId + "#" + " , " + newNote.createdDate + " , " + title + " , " + note + " , " + "\n");
             System.out.println("Note Successfully Created!");
         } catch (IOException e) {
             System.out.println("An Error Occurred, Try Again later!");
         }
+    }
+
+    int GetNextId() {
+        int noteLines = 0;
+
+        try (BufferedReader noteReader = new BufferedReader(new FileReader("notes.txt"))) {
+            while(noteReader.readLine() != null) {
+                noteLines++;
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error in accessing (notes.txt)!");
+        }
+        return  noteLines;
     }
 
     void DeleteNote() {
@@ -31,6 +46,37 @@ public class NoteManager {
     }
 
     void ReadNote() {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("----------------------------");
+        System.out.println("Read Note");
+        DisplayAllNote();
+        System.out.println("Enter Note Id: ");
+        int nNote = userInput.nextInt();
+
+        try (BufferedReader noteReader = new BufferedReader(new FileReader("notes.txt"))) {
+
+            String noteLine;
+
+            while ((noteLine = noteReader.readLine()) != null) {
+                String[] noteCheck = noteLine.split(" , ");
+                int id = Integer.parseInt(noteCheck[0].replace("#", ""));
+
+                if (id == nNote) {
+                    System.out.println("");
+                    System.out.println("ID: " + id);
+                    System.out.println("Date Created: " + noteCheck[1]);
+                    System.out.println("Title: " + noteCheck[2]);
+                    System.out.println("------------------------------");
+                    System.out.println(noteCheck[3]);
+                    System.out.println("------------------------------");
+                }
+                System.out.println("Error!");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error in accessing (notes.txt)!");
+        }
 
     }
 
@@ -39,12 +85,26 @@ public class NoteManager {
     }
 
     void DisplayAllNote() {
+        System.out.println("Your Notes:");
+
+        try (BufferedReader noteReader = new BufferedReader(new FileReader("notes.txt"))) {
+
+            String noteLine;
+
+            while ((noteLine = noteReader.readLine()) != null) {
+                String separator[] = noteLine.split(", ");
+
+                String noteId = separator [0].replace("#", "");
+                String title = separator [2];
+
+                System.out.println("ID: "+ noteId + "|" + " Title: " + title);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error in accessing (notes.txt)!");
+        }
+
+
 
     }
-
-
-
-
-
-
 }
